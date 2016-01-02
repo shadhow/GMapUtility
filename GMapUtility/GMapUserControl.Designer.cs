893ead4235b7d10d146cd1efe7e1891ec1bb069e
shadhow <shdhoward@gmail.com>
2016-01-03 05:37:14 +0800

    Initial commit of GMapUtility namespace

---
+++ GMapUtility/GMapUserControl.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace GMapUtility
+{
+    partial class GMapUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StatusStrip = new System.Windows.Forms.StatusStrip();
+            this.CoordsLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.StatusMessageLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.GMapVersionLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.OptionsPanel = new System.Windows.Forms.Panel();
+            this.ZoomCentreCheckBox = new System.Windows.Forms.CheckBox();
+            this.ZoomTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ProviderComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.splitContainer = new System.Windows.Forms.SplitContainer();
+            this.OverlayListBox = new System.Windows.Forms.ListBox();
+            this.gMap = new GMap.NET.WindowsForms.GMapControl();
+            this.StatusStrip.SuspendLayout();
+            this.OptionsPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
+            this.splitContainer.Panel1.SuspendLayout();
+            this.splitContainer.Panel2.SuspendLayout();
+            this.splitContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // StatusStrip
+            // 
+            this.StatusStrip.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.StatusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.CoordsLabel,
+            this.StatusMessageLabel,
+            this.GMapVersionLabel});
+            this.StatusStrip.Location = new System.Drawing.Point(0, 407);
+            this.StatusStrip.Name = "StatusStrip";
+            this.StatusStrip.Size = new System.Drawing.Size(486, 22);
+            this.StatusStrip.TabIndex = 2;
+            this.StatusStrip.Text = "statusStrip1";
+            // 
+            // CoordsLabel
+            // 
+            this.CoordsLabel.Name = "CoordsLabel";
+            this.CoordsLabel.Size = new System.Drawing.Size(90, 17);
+            this.CoordsLabel.Text = "[mouse coords]";
+            // 
+            // StatusMessageLabel
+            // 
+            this.StatusMessageLabel.Name = "StatusMessageLabel";
+            this.StatusMessageLabel.Size = new System.Drawing.Size(95, 17);
+            this.StatusMessageLabel.Text = "[status message]";
+            // 
+            // GMapVersionLabel
+            // 
+            this.GMapVersionLabel.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Italic);
+            this.GMapVersionLabel.Name = "GMapVersionLabel";
+            this.GMapVersionLabel.Size = new System.Drawing.Size(286, 17);
+            this.GMapVersionLabel.Spring = true;
+            this.GMapVersionLabel.Text = "GMap.NET 1.7.1";
+            this.GMapVersionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // OptionsPanel
+            // 
+            this.OptionsPanel.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.OptionsPanel.Controls.Add(this.ZoomCentreCheckBox);
+            this.OptionsPanel.Controls.Add(this.ZoomTextBox);
+            this.OptionsPanel.Controls.Add(this.label2);
+            this.OptionsPanel.Controls.Add(this.ProviderComboBox);
+            this.OptionsPanel.Controls.Add(this.label1);
+            this.OptionsPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.OptionsPanel.Location = new System.Drawing.Point(0, 0);
+            this.OptionsPanel.Name = "OptionsPanel";
+            this.OptionsPanel.Size = new System.Drawing.Size(486, 27);
+            this.OptionsPanel.TabIndex = 4;
+            // 
+            // ZoomCentreCheckBox
+            // 
+            this.ZoomCentreCheckBox.AutoSize = true;
+            this.ZoomCentreCheckBox.Location = new System.Drawing.Point(309, 5);
+            this.ZoomCentreCheckBox.Name = "ZoomCentreCheckBox";
+            this.ZoomCentreCheckBox.Size = new System.Drawing.Size(107, 17);
+            this.ZoomCentreCheckBox.TabIndex = 4;
+            this.ZoomCentreCheckBox.Text = "Zoom and centre";
+            this.ZoomCentreCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // ZoomTextBox
+            // 
+            this.ZoomTextBox.Location = new System.Drawing.Point(284, 3);
+            this.ZoomTextBox.Name = "ZoomTextBox";
+            this.ZoomTextBox.Size = new System.Drawing.Size(19, 20);
+            this.ZoomTextBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(225, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Zoom level";
+            // 
+            // ProviderComboBox
+            // 
+            this.ProviderComboBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Append;
+            this.ProviderComboBox.FormattingEnabled = true;
+            this.ProviderComboBox.Location = new System.Drawing.Point(50, 3);
+            this.ProviderComboBox.Name = "ProviderComboBox";
+            this.ProviderComboBox.Size = new System.Drawing.Size(169, 21);
+            this.ProviderComboBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Provider";
+            // 
+            // splitContainer
+            // 
+            this.splitContainer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainer.Location = new System.Drawing.Point(62, 39);
+            this.splitContainer.Name = "splitContainer";
+            // 
+            // splitContainer.Panel1
+            // 
+            this.splitContainer.Panel1.Controls.Add(this.OverlayListBox);
+            // 
+            // splitContainer.Panel2
+            // 
+            this.splitContainer.Panel2.Controls.Add(this.gMap);
+            this.splitContainer.Size = new System.Drawing.Size(363, 351);
+            this.splitContainer.SplitterDistance = 120;
+            this.splitContainer.TabIndex = 5;
+            // 
+            // OverlayListBox
+            // 
+            this.OverlayListBox.FormattingEnabled = true;
+            this.OverlayListBox.Location = new System.Drawing.Point(14, 53);
+            this.OverlayListBox.Name = "OverlayListBox";
+            this.OverlayListBox.Size = new System.Drawing.Size(88, 108);
+            this.OverlayListBox.TabIndex = 0;
+            // 
+            // gMap
+            // 
+            this.gMap.Bearing = 0F;
+            this.gMap.CanDragMap = true;
+            this.gMap.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMap.GrayScaleMode = false;
+            this.gMap.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMap.LevelsKeepInMemmory = 5;
+            this.gMap.Location = new System.Drawing.Point(49, 83);
+            this.gMap.MarkersEnabled = true;
+            this.gMap.MaxZoom = 40;
+            this.gMap.MinZoom = 1;
+            this.gMap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionWithoutCenter;
+            this.gMap.Name = "gMap";
+            this.gMap.NegativeMode = false;
+            this.gMap.PolygonsEnabled = true;
+            this.gMap.RetryLoadTile = 0;
+            this.gMap.RoutesEnabled = true;
+            this.gMap.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Fractional;
+            this.gMap.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMap.ShowTileGridLines = false;
+            this.gMap.Size = new System.Drawing.Size(129, 101);
+            this.gMap.TabIndex = 2;
+            this.gMap.Zoom = 0D;
+            // 
+            // GMapUserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.splitContainer);
+            this.Controls.Add(this.OptionsPanel);
+            this.Controls.Add(this.StatusStrip);
+            this.Name = "GMapUserControl";
+            this.Size = new System.Drawing.Size(486, 429);
+            this.StatusStrip.ResumeLayout(false);
+            this.StatusStrip.PerformLayout();
+            this.OptionsPanel.ResumeLayout(false);
+            this.OptionsPanel.PerformLayout();
+            this.splitContainer.Panel1.ResumeLayout(false);
+            this.splitContainer.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
+            this.splitContainer.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip StatusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel CoordsLabel;
+        private System.Windows.Forms.ToolStripStatusLabel StatusMessageLabel;
+        private System.Windows.Forms.ToolStripStatusLabel GMapVersionLabel;
+        private System.Windows.Forms.Panel OptionsPanel;
+        private System.Windows.Forms.CheckBox ZoomCentreCheckBox;
+        private System.Windows.Forms.TextBox ZoomTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox ProviderComboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.SplitContainer splitContainer;
+        private System.Windows.Forms.ListBox OverlayListBox;
+        private GMap.NET.WindowsForms.GMapControl gMap;
+    }
+}
---
+++ GMapUtility/GMapUserControl.cs
@@ -0,0 +1,353 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using GMap.NET;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using GMap.NET.MapProviders;
+using Catfood.Shapefile;
+
+namespace GMapUtility
+{
+    public partial class GMapUserControl : UserControl
+    {
+        //public properties
+        #region properties
+        public Exception ErrorMessage { get; private set; }
+
+        public bool TopPanelVisible { get; private set; }
+        public bool StatusStripVisible { get; private set; }
+        public bool LegendPanelVisible { get; private set; }
+
+        public string CurrentProvider { get; set; }
+        public string PositionKeyword { get; set; }
+        public int CurrentZoomLevel { get; set; }
+
+        public List<string> GMapProvidersList { get; private set; }  //TODO: make  provider list 
+        public List<string> OverlayList { get; private set; }
+        public List<string> MarkerList { get; private set; }
+
+        #endregion properties
+
+        //TODO: generalise for multiple overlays and markers; lines and polygons
+        #region declarations/private properties
+        private List<GMapOverlay> gMapOverlayList = new List<GMapOverlay>();
+
+        public int MarkerSize { get; set; }
+        public Color MarkerColour { get; set; }
+
+
+        GMapOverlay overlay;
+        GMarkerGoogle marker;
+        Bitmap bitmap;
+        Color markerColour = Color.Red;
+        int markerSize = 2;
+
+        Shapefile myShapefile;
+        List<ShapePoint> shapePoint;
+
+        DataTable myDataTable;
+        #endregion declarations/private properties
+
+        //constructor
+        #region constructor
+        public GMapUserControl()
+        {
+            InitializeComponent();
+
+            DoFormLayout();
+
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly;
+
+            SetProvidersList();
+            SetDefaults();
+        }
+
+        private void DoFormLayout()
+        {
+            splitContainer.Dock = DockStyle.Fill;
+            splitContainer.Panel1Collapsed = true;
+            gMap.Dock = DockStyle.Fill;
+
+            OverlayListBox.Dock = DockStyle.Fill;
+
+        }
+
+        private void SetDefaults()
+        {
+
+            //set position
+            if (string.IsNullOrEmpty(PositionKeyword))
+                PositionKeyword = "Western Australia";
+
+            gMap.SetPositionByKeywords(PositionKeyword);
+
+            //zoomlevel
+            if (CurrentZoomLevel == 0)
+                CurrentZoomLevel = 4;
+
+            gMap.Zoom = CurrentZoomLevel;
+            gMap_OnMapZoomChanged();
+
+            //provider
+            ProviderComboBox.SelectedIndex = ProviderComboBox.FindStringExact(CurrentProvider);
+
+            StatusMessageLabel.Text = "";
+
+            gMap.ShowCenter = false;
+            gMap.DragButton = MouseButtons.Left;
+
+            //overlay
+            OverlayList = new List<string>();
+            overlay = new GMapOverlay("markers");
+
+
+            //markers
+            MarkerColour = markerColour;
+            MarkerSize = markerSize;
+            bitmap = CreateBitmap(MarkerSize, MarkerColour);
+
+            ShowTopPanel(true);
+            ShowStatusStrip(true);
+
+
+            //data
+            myShapefile = new Shapefile();
+            myDataTable = new DataTable();
+        }
+
+        private void SetProvidersList()
+        {
+            ProviderComboBox.SelectedIndexChanged -= ProviderComboBox_SelectedIndexChanged;
+            ProviderComboBox.DataSource = GMapProviders.List;
+            ProviderComboBox.SelectedIndexChanged += ProviderComboBox_SelectedIndexChanged;
+
+            if (string.IsNullOrEmpty(CurrentProvider))
+                CurrentProvider = "BingSatelliteMap";
+
+        }
+        #endregion constructor
+
+        //public methods
+        #region public methods
+
+        //overlays
+        public void AddOverlay(string name)
+        {
+            if (OverlayList.Any(str => str.Equals(name)))       //name already in list
+                return;
+
+            var overlay = new GMapOverlay(name);
+            gMapOverlayList.Add(overlay);
+            OverlayList.Add(name);
+
+            //OverlayListBox.DataSource = null;
+            OverlayListBox.DataSource = OverlayList;
+        }
+
+        public void DeleteOverlay(string name)
+        {
+            gMapOverlayList.Remove(TheGMapOverlay(name));
+            OverlayList.Remove(name);
+        }
+
+        public void ShowOverlay(string name)
+        {
+            TheGMapOverlay(name).IsVisibile = true;
+            this.Refresh();
+        }
+
+        public void HideOverlay(string name)
+        {
+            //int ndx = OverlayList.IndexOf(name);
+            //gMapOverlayList[ndx].Clear();
+            TheGMapOverlay(name).IsVisibile = false;
+            this.Refresh();
+
+        }
+
+        public void ResetOverlays()
+        {
+            gMap.Overlays.Clear();
+            foreach (GMapOverlay overlay in gMapOverlayList)
+                overlay.Clear();
+        }
+
+        //status
+        public void ShowTopPanel(bool on)
+        {
+            OptionsPanel.Visible = on;
+            TopPanelVisible = on;
+            //this.Refresh();
+        }
+
+        public void ShowStatusStrip(bool on)
+        {
+            StatusStrip.Visible = on;
+            StatusStripVisible = on;
+        }
+
+        public void ShowLegendPanel(bool on)
+        {
+            splitContainer.Panel1Collapsed = !on;
+            LegendPanelVisible = on;
+        }
+
+        //plotting markers
+        public void PlotShapefile(string shapeFile, string overlay, string marker, Color markerColour)
+        {
+            ErrorMessage = null;
+            myShapefile = ReadShapefile(shapeFile);
+
+            //ReadShapefile(shapeFile);
+            if (ErrorMessage != null)
+                return;
+
+            StatusMessageLabel.Text = myShapefile.Count.ToString() + " " + myShapefile.Type.ToString() + "s";
+
+
+            MarkerColour = markerColour;
+
+            if (myShapefile.Type == ShapeType.Point)
+                ShowShapePoints(TheGMapOverlay(overlay));
+
+            this.Refresh();
+        }
+
+        public void PlotDataTable(DataTable dataTable, string overlay, string marker, Color markerColour)
+        {
+            ErrorMessage = null;
+            MarkerColour = markerColour;
+
+            PlotDataPoints();
+            gMap.Refresh();
+
+        }
+
+
+        #endregion public methods
+
+        //events
+        private void ProviderComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            gMap.MapProvider = (GMapProvider)ProviderComboBox.SelectedItem;
+            gMap.Refresh();
+        }
+
+        private void gMap_MouseMove(object sender, MouseEventArgs e)
+        {
+            //http://stackoverflow.com/questions/18874868/how-to-return-lat-long-with-mousemove-in-gmap-net
+
+            base.OnMouseMove(e);
+
+            double X = gMap.FromLocalToLatLng(e.X, e.Y).Lng;
+            double Y = gMap.FromLocalToLatLng(e.X, e.Y).Lat;
+
+            string longitude = X.ToString(@"#.##0");
+            string latitude = Y.ToString(@"#.##0");
+            CoordsLabel.Text = "Coords: [" + longitude + ", " + latitude + "]";
+        }
+
+        private void gMap_OnMapZoomChanged()
+        {
+            ZoomTextBox.Text = gMap.Zoom.ToString();
+            Size size = new System.Drawing.Size(splitContainer.Panel2.Width, splitContainer.Panel2.Height);
+            gMap.ClientSize = size;
+            this.Refresh();
+        }
+
+        private void ZoomCentreCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            gMap.MouseWheelZoomType = (ZoomCentreCheckBox.Checked) ? MouseWheelZoomType.MousePositionAndCenter :
+                                                                            MouseWheelZoomType.MousePositionWithoutCenter;
+        }
+
+        private void gMap_MouseLeave(object sender, EventArgs e)
+        {
+            CoordsLabel.Text = "";
+        }
+
+        //utility methods
+        private Bitmap CreateBitmap(int side, Color colour)
+        {
+            //http://www.techotopia.com/index.php/Using_Bitmaps_for_Persistent_Graphics_in_C_Sharp
+
+            var myBitmap = new Bitmap(side, side);
+            using (Graphics g = Graphics.FromImage(myBitmap))
+            {
+                using (Brush b = new SolidBrush(colour))
+                {
+                    g.FillRectangle(b, 0, 0, side, side);
+                }
+            }
+
+            return myBitmap;
+        }
+
+        private GMapOverlay TheGMapOverlay(string overlay)
+        {
+            int ndx = OverlayList.IndexOf(overlay);
+            return gMapOverlayList[ndx];
+        }
+
+        private Shapefile ReadShapefile(string shapefile)
+        {
+            var catfoodShapefile = new Shapefile();
+            try
+            {
+                catfoodShapefile.Open(shapefile);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, "Error : " + ex.Message);
+                ErrorMessage = ex;
+            }
+            return catfoodShapefile;
+        }
+
+        private void ShowShapePoints(GMapOverlay overlay)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            ShapePoint shapePoint;
+
+            myShapefile.Reset();
+            gMap.Overlays.Clear();
+            //overlay.Clear();
+
+            //ResetOverlay();
+
+            for (int i = 0; i < myShapefile.Count; i++)
+            {
+                if (myShapefile.MoveNext())
+                {
+                    shapePoint = (ShapePoint)myShapefile.Current;
+
+                    marker = new GMarkerGoogle(new GMap.NET.PointLatLng(shapePoint.Point.Y, shapePoint.Point.X),
+                                    bitmap);
+
+                    overlay.Markers.Add(marker);
+                }
+            }
+
+            gMap.Overlays.Add(overlay);
+            gMap.Refresh();
+
+            Cursor.Current = Cursors.Default;
+        }
+
+        private void PlotDataPoints()
+        {
+            MessageBox.Show("not available");
+        }
+
+
+
+    }
+}
